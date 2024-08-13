86ba33eb68120513fcab151afe1cfab068d5076b
gichobih <dennismuri254@gmail.com>
2024-08-13 02:21:58 -0700

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,34 +1,18 @@
 #!/usr/bin/python3
-"""
-Module to query the number of subscribers on a given Reddit subreddit.
-"""
-
-import requests
+"""Module for task 0"""
 
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers
-    for a given subreddit.
-    
-    Args:
-        subreddit (str): The name of the subreddit.
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+    import requests
 
-    Returns:
-        int: The number of subscribers, or 0 if the subreddit is invalid.
-    """
-    if not isinstance(subreddit, str) or subreddit == "":
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
         return 0
-    
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        try:
-            data = response.json()
-            return data.get("data", {}).get("subscribers", 0)
-        except ValueError:
-            return 0
-    return 0
+    return sub_info.json().get("data").get("subscribers")
 
