0732bd3439c9de5d23143e31fa5dfa22b526768e
gichobih <dennismuri254@gmail.com>
2024-08-13 06:57:45 -0700

    0-custom_http_response_header

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
+
 """
-Script that queries subscribers on a given Reddit subreddit.
+Function that queries the Reddit API and returns the number of subscribers
+for a given subreddit.
 """
 
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """
+    Returns the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers, or 0 if the subreddit is invalid.
+    """
+    usr = {"User-Agent": "custom-user-agent"}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    response = requests.get(url, headers=usr, allow_redirects=False)
+
+    if response.status_code != 200:
+        return 0
+
+    try:
+        data = response.json().get("data", {})
+        return data.get("subscribers", 0)
+    except (KeyError, TypeError, ValueError):
         return 0
 
