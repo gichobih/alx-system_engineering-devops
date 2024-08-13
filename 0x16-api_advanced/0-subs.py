e376105e8f0380ccfef7da0832c8b3a7b643ede5
gichobih <dennismuri254@gmail.com>
2024-08-13 02:13:53 -0700

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,15 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """
+    Return the total number of subscribers on a given subreddit.
+    
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers to the subreddit, or 0 if not found.
+    """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
@@ -17,3 +25,4 @@ def number_of_subscribers(subreddit):
         return subscribers
     else:
         return 0
+
