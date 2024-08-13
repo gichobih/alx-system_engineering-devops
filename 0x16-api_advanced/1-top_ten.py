cee19fcab92a8663209a32f9f442172b3e7688ec
gichobih <dennismuri254@gmail.com>
2024-08-13 06:27:32 -0700

    0-custom_http_response_header

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-function that queries the Reddit API and prints the 
+Function that queries the Reddit API and prints the 
 titles of the first 10 hot posts listed for a given subreddit.
 """
 
@@ -9,23 +9,23 @@ import requests
 from sys import argv
 
 def top_ten(subreddit):
-
-   """
-   Returns top ten posts from a given subreddit.
-   """
-   usr = {"User-Agent": "lizzie"}
-   url = requests.get(
-           "https://www.reddit.com/r/{}/hot/.json?limit=10".format(subreddit),
-           headers = usr,
-           allow_redirect= False
+    """
+    Returns the top ten posts from a given subreddit.
+    """
+    usr = {"User-Agent": "lizzie"}
+    url = requests.get(
+        "https://www.reddit.com/r/{}/hot/.json?limit=10".format(subreddit),
+        headers=usr,
+        allow_redirects=False
     ).json()
 
-   try:
-       for post in url.get("data").get("children"):
-           print(post.get("data").get("title"))
-   except (KeyError, TypeError):
-       print("None")
+    try:
+        for post in url.get("data").get("children"):
+            print(post.get("data").get("title"))
+    except (KeyError, TypeError):
+        print("None")
 
 
 if __name__ == "__main__":
     top_ten(argv[1])
+
