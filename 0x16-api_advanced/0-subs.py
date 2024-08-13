d233dc70d960bc000a013bc53ec1b3278a424223
gichobih <dennismuri254@gmail.com>
2024-08-13 02:19:59 -0700

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,18 +10,25 @@ def number_of_subscribers(subreddit):
     """
     Queries the Reddit API and returns the number of subscribers
     for a given subreddit.
+    
+    Args:
+        subreddit (str): The name of the subreddit.
 
     Returns:
         int: The number of subscribers, or 0 if the subreddit is invalid.
     """
+    if not isinstance(subreddit, str) or subreddit == "":
+        return 0
+    
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        data = response.json()
-        subscribers = data.get("data", {}).get("subscribers", 0)
-        return subscribers
-    else:
-        return 0
+        try:
+            data = response.json()
+            return data.get("data", {}).get("subscribers", 0)
+        except ValueError:
+            return 0
+    return 0
 
