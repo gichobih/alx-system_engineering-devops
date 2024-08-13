b691bbbc17563f77fa9046b2ea9e5ed04338a4bc
gichobih <dennismuri254@gmail.com>
2024-08-13 02:17:32 -0700

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script that queries subscribers on a given Reddit subreddit.
+Module to query the number of subscribers on a given Reddit subreddit.
 """
 
 import requests
@@ -8,20 +8,19 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    Return the total number of subscribers on a given subreddit.
-    
-    Args:
-        subreddit (str): The name of the subreddit.
+    Queries the Reddit API and returns the number of subscribers
+    for a given subreddit.
 
     Returns:
-        int: The number of subscribers to the subreddit, or 0 if not found.
+        int: The number of subscribers, or 0 if the subreddit is invalid.
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code == 200:
         data = response.json()
-        subscribers = data['data']['subscribers']
+        subscribers = data.get("data", {}).get("subscribers", 0)
         return subscribers
     else:
         return 0
