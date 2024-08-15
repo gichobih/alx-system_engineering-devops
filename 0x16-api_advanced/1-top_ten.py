bf60243af299a0f20345fa270090f4d12f81f912
gichobih <dennismuri254@gmail.com>
2024-08-15 03:41:37 -0700

    0x16-api_advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,38 +1,24 @@
 #!/usr/bin/python3
-
-"""
-Function that queries the Reddit API and prints the 
-titles of the first 10 hot posts listed for a given subreddit.
-"""
-
+'''
+    this module contains the function top_ten
+'''
 import requests
+from sys import argv
+
 
 def top_ten(subreddit):
-    """
-    Prints the titles of the first 10 hot posts listed for a given subreddit.
-    
-    Args:
-        subreddit (str): The name of the subreddit.
-    
-    Returns:
-        None
-    """
-    usr = {"User-Agent": "custom-user-agent"}
-    url = "https://www.reddit.com/r/{}/hot/.json?limit=10".format(subreddit)
-    response = requests.get(url, headers=usr, allow_redirects=False)
-
-    if response.status_code != 200:
-        print("None")
-        return
-
+    '''
+        returns the top ten posts for a given subreddit
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
     try:
-        posts = response.json().get("data", {}).get("children", [])
-        if not posts:
-            print("None")
-            return
-
-        for post in posts:
-            print(post.get("data", {}).get("title"))
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
     except Exception:
-        print("None")
+        print(None)
 
+
+if __name__ == "__main__":
+    top_ten(argv[1])
